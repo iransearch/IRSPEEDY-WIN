d9801f26f35bd214116c7c58919628859245d949
iransearch <123422113+iransearch@users.noreply.github.com>
2026-08-22 03:58:00 +0330

    smart: reduce burst observatory timeout to 5s

--- IRSpeedyVPN/Services/Xray/Samples.cs
+++ IRSpeedyVPN/Services/Xray/Samples.cs
@@ -16,7 +16,7 @@ namespace IRSpeedyVPN.Services.Xray
 					""httpMethod"": ""HEAD"",
 					""interval"": ""60m"",
 					""sampling"": 1,
-					""timeout"": ""9s""
+					""timeout"": ""5s""
 				},
 				""subjectSelector"": [
 					""smart-proxy-""
